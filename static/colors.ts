22df1a3376a8d701b41d322ef4670d77ab698e60
Bean <bean-dev@bean-records.local>
2024-12-02 22:45:02 -0500

    Alt Text Buff

--- static/colors.ts
+++ static/colors.ts
@@ -5,9 +5,12 @@ export const burstColors = {
     bgAqua:     "#17947F",
     accRed:     "#E71D36",
     accGreen:   "#A1E887",
-    transGray:  "rgba(105,105,105, 0.6)"
+    transGray:  "rgba(105,105,105, 0.6)",
+    trans2Gray: "rgba(105,105,105, 0.3)"
 }
 export const burstTextColors = {
     white:      "#ffffff",
-    black:      "#000000"
+    black:      "#000000",
+    subtitles:  "rgba(1, 2, 88, 0.5)",
+    transWhite: "rgba(255, 255, 255, 0.95)"
 }
