31da538e7a2495d0dec0a64a4ea72a636ff83bf7
Ahmed Usman <usmanaa@umich.edu>
2024-12-12 01:18:26 -0500

    better code blocks

--- components/BlogRendererSS.tsx
+++ components/BlogRendererSS.tsx
@@ -1,52 +1,16 @@
-import { Tokens, Marked } from "marked";
+import { Tokens, Marked, Renderer } from "marked";
 import { markedSmartypantsLite } from "marked-smartypants-lite";
 import { markedEmoji, MarkedEmojiOptions } from "marked-emoji";
 import { markedHighlight } from "marked-highlight";
-import hljsJS from "highlight.js/lib/languages/javascript";
-import hljsBash from "highlight.js/lib/languages/bash";
-import hljs from "highlight.js/lib/core";
-hljs.registerLanguage("javascript", hljsJS);
-hljs.registerLanguage("bash", hljsBash);
-const marked = new Marked(
-  markedHighlight({
-    emptyLangClass: "hljs",
-    highlight: (code, lang) => {
-      const language = hljs.getLanguage(lang) ? lang : "plaintext";
-      const highlighted = hljs.highlight(code, { language }).value;
-
-      // Explicitly add langPrefix manually if not handled
-      return `<pre data-component="CopyableCodeBlock" data-lang="${language}" data-code="${encodeURIComponent(code)}"><code class="hljs language-${language}">${highlighted}</code></pre>`;
-    },
-  })
-);
-
-
-
-import { Octokit } from "@octokit/rest";
-
-
-
-const octokit = new Octokit();
-const res = await octokit.rest.emojis.get();
-const gitEmojis = res.data;
-
-// Custom emoji set
-const customEmojis = {
-  "arch_linux":
-    "https://github.com/archlinux/archweb/blob/master/sitestatic/favicon.png?raw=true",
+import hljs from "highlight.js";
+const customRenderer = new Renderer();
+customRenderer.link = ({ href, title, text }): string => {
+  const safeHref = href ?? "#";
+  const safeTitle = title ? `title="${title}"` : "";
+  text = marked.parseInline(text) as string;
+  return `<a href="${safeHref}" ${safeTitle} target="_blank" rel="noopener noreferrer">${text}</a>`;
 };
-
-const options: MarkedEmojiOptions = {
-  emojis: { ...gitEmojis, ...customEmojis },
-  renderer: (token) =>
-    `<img alt="${token.name}" src="${token.emoji}" class="marked-emoji-img">`,
-};
-
-const renderer = new marked.Renderer();
-marked.use(markedSmartypantsLite(), markedEmoji(options));
-marked.setOptions({ renderer });
-
-renderer.image = ({ href, text }: Tokens.Image): string => {
+customRenderer.image = ({ href, text }: Tokens.Image): string => {
   const ogalt: string = text;
   text = marked.parseInline(text) as string;
   return `
@@ -56,8 +20,7 @@ renderer.image = ({ href, text }: Tokens.Image): string => {
   </figure>
 `;
 };
-
-renderer.heading = ({ text, depth }: Tokens.Heading): string => {
+customRenderer.heading = ({ text, depth }: Tokens.Heading): string => {
   const tag = `h${depth}`;
   text = marked.parseInline(text) as string;
   switch (depth) {
@@ -75,13 +38,43 @@ renderer.heading = ({ text, depth }: Tokens.Heading): string => {
   }
 };
 
-// Customize link rendering to add target and rel attributes
-renderer.link = ({ href, title, text }): string => {
-  const safeHref = href ?? "#";
-  const safeTitle = title ? `title="${title}"` : "";
-  text = marked.parseInline(text) as string;
-  return `<a href="${safeHref}" ${safeTitle} target="_blank" rel="noopener noreferrer">${text}</a>`;
+const marked = new Marked(
+  markedHighlight({
+    emptyLangClass: "hljs",
+    langPrefix: "hljs language-",
+    highlight: (code, lang) => {
+      const language = hljs.getLanguage(lang) ? lang : "plaintext";
+      const highlighted = hljs.highlight(code, { language }).value;
+
+      return `<div data-lang="${language}" data-code="${encodeURIComponent(code)}">${highlighted}</div>`;
+    },
+  }),
+  {
+    renderer: customRenderer
+  }
+);
+
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit();
+const res = await octokit.rest.emojis.get();
+const gitEmojis = res.data;
+
+// Custom emoji set
+const customEmojis = {
+  "arch_linux":
+    "https://github.com/archlinux/archweb/blob/master/sitestatic/favicon.png?raw=true",
 };
+
+const options: MarkedEmojiOptions = {
+  emojis: { ...gitEmojis, ...customEmojis },
+  renderer: (token) =>
+    `<img alt="${token.name}" src="${token.emoji}" class="marked-emoji-img">`,
+};
+
+
+marked.use(markedSmartypantsLite(), markedEmoji(options));
+
 
 export default function BlogPostRenderer({ content }: { content: string }) {
   const renderedContent = marked.parse(
