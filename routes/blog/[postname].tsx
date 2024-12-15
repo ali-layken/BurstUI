02005518a38e104cf1cda1a59b743edc613ea308
Bean <bean-dev@bean-records.local>
2024-12-15 18:21:50 -0500

    404

--- routes/blog/[postname].tsx
+++ routes/blog/[postname].tsx
@@ -1,40 +1,67 @@
 import DynamicMarkdownItem from "../../islands/DynamicMarkdownItem.tsx";
 import BlogPostRenderer from "../../components/BlogRendererSS.tsx";
 import BackButton3D from "../../islands/BackButton3D.tsx";
-
 import { readFileSync, statSync } from "node:fs";
 import { join } from "node:path";
-import { PageProps } from "$fresh/server.ts";
-import Error404 from "../_404.tsx";
+import { Handlers, PageProps } from "$fresh/server.ts";
 
-export default function Blog(props: PageProps) {
-  const filePath = join(Deno.cwd(), "posts", `${props.params.postname}.md`);
-  let content;
+interface BlogProps {
+  content: string;
+  createdTime: string;
+  modifiedTime: string;
+  title: string;
+}
+
+export const handler: Handlers = {
+  GET(_req, ctx) {
+    const blogpost: BlogProps | null = fetchBlogpost(ctx.params.postname);
+    if (!blogpost) {
+      return ctx.renderNotFound({
+        custom: "prop",
+      });
+    }
+    return ctx.render({blogpost });
+  },
+};
+
+const fetchBlogpost = (slug: string): BlogProps | null => {
+  const filePath = join(Deno.cwd(), "posts", `${slug}.md`);
+  let content: string;
   let createdTime;
   let modifiedTime;
 
   try {
-    content = readFileSync(filePath, "utf-8");
-
     const fileStats = statSync(filePath);
+
+    content = readFileSync(filePath, "utf-8");
     createdTime = new Date(fileStats.birthtime).toLocaleString();
     modifiedTime = new Date(fileStats.mtime).toLocaleString();
-  } catch (_error) {
-    return Error404(props);
+  } catch {
+    return null;
   }
 
   // Use the parameter as the title
-  const title = props.params.postname.replace(/_/g, " ").toUpperCase();
+  const title = slug.replace(/_/g, " ").toUpperCase();
+  return {
+    content: content,
+    createdTime: createdTime,
+    modifiedTime: modifiedTime,
+    title: title
+  }
+}
 
+export default function Blog(props: PageProps) {
+
+  const postProps = props.data.blogpost
   // Parse Markdown server-side
-  const renderedMarkdown = BlogPostRenderer({ content });
+  const renderedMarkdown = BlogPostRenderer(postProps.content);
 
   return (
     <>
       <header class="mb-4">
-        <h1 class="text-5xl font-bold mb-2">{title}</h1>
+        <h1 class="text-5xl font-bold mb-2">{postProps.title}</h1>
         <p class="text-sm text-subtitles">
-          Created: {createdTime} | Last Edited: {modifiedTime}
+          Created: {postProps.createdTime} | Last Edited: {postProps.modifiedTime}
         </p>
       </header>
       {renderedMarkdown}
