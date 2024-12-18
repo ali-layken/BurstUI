c6d55fecffdce2e8262e8be3efb3e60a0e98324c
Bean <bean-dev@bean-records.local>
2024-12-18 00:44:10 -0500

    heading nav wip

--- components/BlogRendererSS.tsx
+++ components/BlogRendererSS.tsx
@@ -5,8 +5,17 @@ import { markedHighlight } from "marked-highlight";
 import hljs from "highlight.js";
 import { Octokit } from "@octokit/rest";
 import { burstColors, burstTextColors } from "../static/colors.ts";
+import { JSX } from "preact/jsx-runtime";
+
+export interface HeadingInfo { id: string; text: string; level: number }
+interface RendererResult {
+  renderedContent: JSX.Element;
+  headings: HeadingInfo[];
+}
 
 const customRenderer = new Renderer();
+const headings: HeadingInfo[] = [];
+
 customRenderer.link = ({ href, title, text }): string => {
   try{
     const safeHref = href ?? "#";
@@ -31,23 +40,29 @@ customRenderer.image = ({ href, text }: Tokens.Image): string => {
 };
 customRenderer.heading = ({ text, depth }: Tokens.Heading): string => {
   const tag = `h${depth}`;
+  const id = text
+  .toLowerCase()
+  .replace(/\s+/g, "-")
+  .replace(/[^a-z0-9-]/g, ""); // Generate unique ID for TOC
   text = marked.parseInline(text) as string;
+  const tocHtml = text.replace(/<a[^>]*>(.*?)<\/a>/g, "$1");
+  headings.push({ id, text: tocHtml, level: depth }); // Add heading to TOC array
+
   switch (depth) {
     case 3:
       return `
         <div style="height: 1rem; display: block;"></div>
-        <${tag}>${text}</${tag}>
-        <hr style="width: 75%; margin: 0rem 0 0.5rem; text-align: left;" />
+        <${tag} id="${id}" class="group">${text}<a href="#${id}" class="ml-2 text-subtitles text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300">🔗</a><hr style="width: 75%; margin: 0rem 0 0.5rem; text-align: left;" /></${tag}>
     `;
     case 4:
       return `
         <div style="height: 1rem; display: block;"></div>
-        <${tag}>${text}</${tag}>
+        <${tag} id="${id}" class="group">${text}<a href="#${id}" class="ml-2 text-subtitles text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300">🔗</a></${tag}>
     `;
     default:
       return `
       <div style="height: 1rem; display: block;"></div>
-      <${tag}>${text}</${tag}>
+      <${tag} id="${id}" class="group">${text}<a href="#${id}" class="ml-2 text-subtitles text-sm opacity-0 group-hover:opacity-100 transition-opacity duration-300">🔗</a></${tag}>
       `;
   }
 };
@@ -106,16 +121,19 @@ const options: MarkedEmojiOptions = {
 marked.use(markedSmartypantsLite(), markedEmoji(options));
 
 
-export default function BlogPostRenderer(content: string) {
+export default function BlogPostRenderer(content: string): RendererResult {
+  headings.length = 0;
   const renderedContent = marked.parse(
     content.replace(/{{(\w+)}}/g, (_match, componentName) => {
       return `<div id="component-${componentName}" data-component="${componentName}"></div>`;
     }),
   );
 
-  return (
-    <article class="prose lg:prose-xl custom-prose mx-auto">
-      <div dangerouslySetInnerHTML={{ __html: renderedContent as string }} />
-    </article>
-  );
+  return {
+    renderedContent: (
+      <article class="prose lg:prose-xl custom-prose mx-auto">
+        <div dangerouslySetInnerHTML={{ __html: renderedContent as string }} />
+      </article>
+    ),
+    headings: headings};
 }
