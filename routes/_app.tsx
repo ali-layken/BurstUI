c6a45a66e01a4515d5e84f1dcb143fe0d665db4a
Bean <bean-dev@bean-records.local>
2024-12-19 01:13:41 -0500

    Dynamically sized

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,10 @@
 import { type PageProps } from "$fresh/server.ts";
 import { JSX } from "preact/jsx-runtime";
-import SiteNav from "../islands/SiteNav.tsx";
-import { useRef } from "preact/hooks"; 
+import DynamicLayout from "../islands/DynamicLayout.tsx";
+import ResizeDetector from "../islands/ResizeDetector.tsx";
 import { linklist } from "../utils/linklist.ts";
 
-export default function App({ Component, route, }: PageProps) {
+export default function App({ Component, route }: PageProps): JSX.Element {
   return (
     <html>
       <head>
@@ -14,26 +14,12 @@ export default function App({ Component, route, }: PageProps) {
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
-        {/* Outer wrapper to maintain purple background */}
         <div class="min-h-screen bg-bgPurple flex flex-col items-center justify-start">
-          {/* Add space above */}
-          <div class="h-12 bg-bgPurple"></div>
-
-          {/* Flex container */}
-          <div class="max-w-full w-full px-4 flex gap-8 justify-center">
-            {/* Left smaller div */}
-            <div class="flex-2 px-4 py-4 top-24 bg-bgAqua rounded-md shadow-lg self-start sticky flex items-center justify-center">
-              <SiteNav currentPage={route} headingsSignal={linklist}/>
-            </div>
-            {/* Right larger div */}
-            <div class="flex-1 max-w-4xl px-8 py-8 bg-bgAqua rounded-md shadow-lg">
-              <Component />
-            </div>
-          </div>
-
-          {/* Add space below */}
-          <div class="h-12 bg-bgPurple"></div>
+        <DynamicLayout currentPage={route} headingsSignal={linklist}>
+          <Component />
+        </DynamicLayout>
         </div>
+        <ResizeDetector />
       </body>
     </html>
   );
