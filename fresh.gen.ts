b4bec1bbdac40263282ffb6b04f65f015497e74b
Bean <bean-dev@bean-records.local>
2024-12-21 17:38:10 -0500

    a working state

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -4,6 +4,7 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
+import * as $_layout from "./routes/_layout.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
 import * as $blog_postname_ from "./routes/blog/[postname].tsx";
 import * as $cube from "./routes/cube.tsx";
@@ -22,6 +23,7 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
+    "./routes/_layout.tsx": $_layout,
     "./routes/api/joke.ts": $api_joke,
     "./routes/blog/[postname].tsx": $blog_postname_,
     "./routes/cube.tsx": $cube,
