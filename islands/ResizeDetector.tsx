3ac7ef3fde0ce83a9322ae347a25da67c516411e
Ahmed Usman <usmanaa@umich.edu>
2024-12-22 04:08:25 -0500

    Resize Reactor Core

--- islands/ResizeDetector.tsx
+++ islands/ResizeDetector.tsx
@@ -3,31 +3,28 @@ import { useEffect, useState } from "preact/hooks";
 import { JSX } from "preact/jsx-runtime";
 import SiteNav, { SiteNavProps } from "./SiteNav.tsx";
 import SiteNavNarrow from "./SiteNavNarrow.tsx";
-import { render } from "preact";
-import { Signal } from "@preact/signals";
+import { Fragment, render } from "preact";
 
 interface ResizeDetectorProps extends SiteNavProps {
-  isNarrowSig: Signal<undefined> | Signal<boolean>
+  isNarrowCookie: boolean | undefined
 }
 
-export default function ResizeDetector({ currentPage, headingsSignal, isNarrowSig }: ResizeDetectorProps): JSX.Element {
+export default function ResizeDetector({ currentPage, headingsSignal, isNarrowCookie }: ResizeDetectorProps): JSX.Element {
   if (globalThis.window == undefined){
     return <></>
   }
-  const [isNarrow, setIsNarrow] = useState(isNarrowSig.value);
+
+  const [isNarrow, setIsNarrow] = useState<boolean | undefined>(isNarrowCookie);
 
   useEffect(() => {
     const updateScreenSize = () => {
       const narrow = globalThis.window?.innerWidth <= 1371;
-      isNarrowSig.value = narrow;
       setIsNarrow(narrow);
       document.cookie = `isNarrow=${narrow}; path=/; max-age=31536000;`;
     };
 
     updateScreenSize(); // Initial check
     
-    
-
     globalThis.window.addEventListener("resize", updateScreenSize);
 
     return () => globalThis.window.removeEventListener("resize", updateScreenSize);
@@ -57,8 +54,9 @@ export default function ResizeDetector({ currentPage, headingsSignal, isNarrowSi
       }
       if (bottomNavDiv) {
         bottomNavDiv.classList.remove("h-12", "bg-bgPurple");
-        render(<SiteNavNarrow currentPage={currentPage} headingsSignal={headingsSignal} />, bottomNavDiv,
-      );}
+        bottomNavDiv.classList.add("w-full", "max-w-4xl")
+        render(<SiteNavNarrow currentPage={currentPage} headingsSignal={headingsSignal} />, bottomNavDiv);
+      }
     } else {
       // Wide layout adjustments
       backgroundDiv?.classList.add("justify-start");
@@ -67,16 +65,17 @@ export default function ResizeDetector({ currentPage, headingsSignal, isNarrowSi
         mainDiv.className = "max-w-full w-full px-4 flex gap-8 justify-center";
       }
       if (wideNavDiv) {
-        wideNavDiv.className = "flex-2 px-4 py-4 top-24 bg-bgAqua rounded-md shadow-lg self-start sticky flex items-center justify-center";
+        wideNavDiv.className = "flex-2 min-w-80 px-4 py-4 top-24 bg-bgAqua rounded-md shadow-lg self-start sticky flex items-center justify-center";
         render(
-          <SiteNav currentPage={currentPage} headingsSignal={headingsSignal} />,
-          wideNavDiv)
+            <SiteNav currentPage={currentPage} headingsSignal={headingsSignal} />,
+            wideNavDiv)
       }
       if (componentDiv) {
         componentDiv.className = "flex-1 max-w-4xl px-8 py-8 bg-bgAqua rounded-md shadow-lg";
       }
       if(bottomNavDiv) {
-        bottomNavDiv.innerHTML = ""
+        render(<Fragment />, bottomNavDiv);
+        bottomNavDiv.classList.remove("w-full", "max-w-4xl");
         bottomNavDiv.classList.add("h-12", "bg-bgPurple");
       };
     }
