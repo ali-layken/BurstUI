2fd767e93f867dadd698798a9f937c5b384cd7f2
Ahmed Usman <usmanaa@umich.edu>
2025-01-12 21:25:57 -0500

    localize the emojis

--- components/BlogRendererSS.tsx
+++ components/BlogRendererSS.tsx
@@ -1,6 +1,6 @@
 import { Octokit } from "@octokit/rest";
 import hljs from "highlight.js";
-import  { marked, Renderer, Tokens } from "marked";
+import { marked, Renderer, Tokens } from "marked";
 import { markedEmoji, MarkedEmojiOptions } from "marked-emoji";
 import { markedHighlight } from "marked-highlight";
 import { markedSmartypantsLite } from "marked-smartypants-lite";
@@ -8,8 +8,11 @@ import { JSX } from "preact/jsx-runtime";
 import { TitleHeaderID } from "../routes/blog/[postname].tsx";
 import { burstTextColors } from "../static/colors.ts";
 
-
-export interface HeadingInfo { id: string; text: string; level: number }
+export interface HeadingInfo {
+  id: string;
+  text: string;
+  level: number;
+}
 interface RendererResult {
   renderedContent: JSX.Element;
   headings: HeadingInfo[];
@@ -19,19 +22,20 @@ const customRenderer = new Renderer();
 const headings: HeadingInfo[] = [];
 
 customRenderer.link = ({ href, title, text }): string => {
-  try{
+  try {
     const safeHref = href ?? "#";
     const safeTitle = title ? `title="${title}"` : ``;
     text = marked.parseInline(text) as string;
-    if(text.endsWith('notab')){
-      return `<a href="${safeHref}" ${safeTitle} rel="noopener noreferrer">${text.replace('notab', '')}</a>`;
+    if (text.endsWith("notab")) {
+      return `<a href="${safeHref}" ${safeTitle} rel="noopener noreferrer">${
+        text.replace("notab", "")
+      }</a>`;
     }
     return `<a href="${safeHref}" ${safeTitle} target="_blank" rel="noopener noreferrer">${text}</a>`;
-  } catch (error){
+  } catch (error) {
     console.log(error);
-    return 'Bad Link Format!';
+    return "Bad Link Format!";
   }
-
 };
 customRenderer.image = ({ href, text }: Tokens.Image): string => {
   const ogalt: string = text;
@@ -46,14 +50,14 @@ customRenderer.image = ({ href, text }: Tokens.Image): string => {
 customRenderer.heading = ({ text, depth }: Tokens.Heading): string => {
   const tag = `h${depth}`;
   const id = text
-  .toLowerCase()
-  .replace(/\s+/g, "-")
-  .replace(/[^a-z0-9-]/g, ""); // Generate unique ID for TOC
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9-]/g, ""); // Generate unique ID for TOC
   text = marked.parseInline(text) as string;
   const tocHtml = text.replace(/<a[^>]*>(.*?)<\/a>/g, "$1");
   headings.push({ id, text: tocHtml, level: depth }); // Add heading to TOC array
 
-  const leftPad = `style="padding-left: ${depth}rem"`
+  const leftPad = `style="padding-left: ${depth}rem"`;
 
   switch (depth) {
     case 1:
@@ -76,8 +80,8 @@ const hlExt = markedHighlight({
   emptyLangClass: "hljs",
   langPrefix: "hljs language-",
   highlight: (code, lang) => {
-    if (lang === 'mermaid') {
-      return `<pre class="mermaid invisible" data-component="MermaidBlock">${code}</pre>`
+    if (lang === "mermaid") {
+      return `<pre class="mermaid invisible" data-component="MermaidBlock">${code}</pre>`;
     }
     const language = hljs.getLanguage(lang) ? lang : "plaintext";
     const highlighted = hljs.highlight(code, { language }).value;
@@ -94,15 +98,18 @@ const hlExt = markedHighlight({
                 <span class="pl-2 text-base">${line}</span>
                 <span 
                   class="copy-line-button absolute right-1 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-700 cursor-pointer text-base"
-                  data-line="${encodeURIComponent(code.split("\n")[index] || "")}"
+                  data-line="${
+        encodeURIComponent(code.split("\n")[index] || "")
+      }"
                   title="Copy Button">📋</span>
               </div>`;
     });
 
-    return `<div data-component="CopyableCodeBlock" data-lang="${language}" data-code="${encodeURIComponent(code)}">${linesWithButtons.join("")}</div>`;
+    return `<div data-component="CopyableCodeBlock" data-lang="${language}" data-code="${
+      encodeURIComponent(code)
+    }">${linesWithButtons.join("")}</div>`;
   },
-})
-
+});
 
 const octokit = new Octokit();
 const res = await octokit.rest.emojis.get();
@@ -110,22 +117,14 @@ const gitEmojis = res.data;
 
 // Custom emoji set
 const customEmojis = {
-  "arch_linux":
-    "https://github.com/archlinux/archweb/blob/master/sitestatic/favicon.png?raw=true",
-  "google_logo":
-    "https://www.google.com/favicon.ico",
-  "apple_logo":
-    "https://www.apple.com/favicon.ico",
-  "r_pi":
-    "https://www.raspbian.org/static/favicon.ico",
-  "debian_logo":
-    "https://www.debian.org/favicon.ico",
-  "red_hat_logo":
-    "https://www.redhat.com/favicon.ico",
-  "chromium_logo":
-    "https://raw.githubusercontent.com/chromium/chromium/refs/heads/main/chrome/app/theme/chromium/product_logo_64.png",
-  "podman_logo":
-    "https://podman.io/logos/optimized/podman-3-logo-95w-90h.webp"
+  "arch_linux": "/emojis/arch.png",
+  "google_logo": "/emojis/google.ico",
+  "apple_logo": "/emojis/apple.ico",
+  "r_pi": "/emojis/raspi.ico",
+  "debian_logo": "/emojis/debian.ico",
+  "red_hat_logo": "/emojis/redhat.ico",
+  "chromium_logo": "/emojis/chromium.png",
+  "podman_logo": "/emojis/podman.webp",
 };
 
 const options: MarkedEmojiOptions = {
@@ -134,17 +133,14 @@ const options: MarkedEmojiOptions = {
     `<img alt="${token.name}" src="${token.emoji}" class="marked-emoji-img">`,
 };
 
-
 marked.use(
   hlExt,
   {
-    renderer: customRenderer
-  }, 
+    renderer: customRenderer,
+  },
   markedSmartypantsLite(),
-  markedEmoji(options)
+  markedEmoji(options),
 );
-
-
 
 export default function BlogPostRenderer(content: string): RendererResult {
   headings.length = 0;
@@ -153,12 +149,13 @@ export default function BlogPostRenderer(content: string): RendererResult {
       return `<div id="component-${componentName}" data-component="${componentName}"></div>`;
     }),
   );
-  headings.unshift({  id: TitleHeaderID, text: "(Top)", level: 0})
+  headings.unshift({ id: TitleHeaderID, text: "(Top)", level: 0 });
   return {
     renderedContent: (
       <article class="prose lg:prose-xl custom-prose mx-auto">
         <div dangerouslySetInnerHTML={{ __html: renderedContent as string }} />
       </article>
     ),
-    headings: headings};
+    headings: headings,
+  };
 }
