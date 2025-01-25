6f617ab50294313fcda01615e5b3c360166e9091
Ahmed Usman <usmanaa@umich.edu>
2025-01-25 17:47:18 -0500

    remove extraneous code

--- islands/DynamicMarkdownItem.tsx
+++ islands/DynamicMarkdownItem.tsx
@@ -120,9 +120,7 @@ export default function DynamicMarkdownItem() {
       if (hash) {
         const target = document.querySelector(hash);
         if (target) {
-          setTimeout(() => {
-            target.scrollIntoView({ behavior: "smooth", block: "start" });
-          }, 0); // Allow hydration to complete
+          target.scrollIntoView({ behavior: "smooth", block: "start" });
         }
       }
     };
